b2e031a708e4b360b52d5cccc3429c784decf6fc
MollyYT <94243770+MollyYT@users.noreply.github.com>
2021-11-20 01:59:18 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,16 +10,9 @@ const yardım = new Discord.MessageEmbed()
 .setDescription(`
 
 
-:white_small_square: **=**  \`!kanal-koruma\` : **Kanal Koruma Aç/Kapat**
-:white_small_square: **=**  \`!küfür-engel\`:  **Küfür Engel Aç/Kapat**
-:white_small_square: **=**  \`!reklam-engel\` :  **Reklam Engel Aç/Kapat**
-:white_small_square: **=**  \`!sohbet aç-kapat\` :  **Sohbeti Açıp Kapatırsınız**
-:white_small_square: **=**  \`!ban\`: **Belirttiğiniz Kişiyi Sunucudan Banlarsınız**
-:white_small_square: **=**  \`!unban\`:  **Belirttiğiniz Kişinin Banını Kaldırırsınız**
-:white_small_square: **=**  \`!istatistik\`:  **Botun İstatistiklerini Atar**
-:white_small_square: **=**  \`!temizle\`:  **Belirttiğiniz Sayıda Mesajı Siler**
-:white_small_square: **=**  \`!ping\`:  **Pinginizi Ölçüp Atar**
-:white_small_square: **=**  \`!avatar\`:  **Avatarınızı Atar**
+:white_small_square: **=**  \`m.moderasyon\`:  **moderasyon sekmesini açar**
+:white_small_square: **=**  \`m.eğlence\`:  **eğlence sekmesini açar**
+:white_small_square: **=**  \`m.uptime\`:  **uptime sekmesi açar**
 
 `)
 .setImage("https://cdn.discordapp.com/attachments/740871896614043669/748878433840398367/Baslksz-1.png")
